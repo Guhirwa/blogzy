371ab9ceafcf3e0a1e26f77f9ff4c72770da1387
Guhirwa <christianguhirwa74@gmail.com>
2025-11-24 16:32:39 +0200

    feat(controller): implementing the delete functionality
    - provide to the use the complete set of responce for each usecaseO

--- backend/controller/BlogController.js
+++ backend/controller/BlogController.js
@@ -13,8 +13,10 @@ const getAllBlogs = async (req, res) => {
 
 const getBlogById = async (req, res) => {
 
+    const blogToSearch = req.params.id;
+
     try{
-        const blog = await Blog.findById(req.params.id);
+        const blog = await Blog.findById(blogToSearch);
         if(!blog) {
             res.status(200).json({success: true, message: "Blog not found"});
         } else {
@@ -45,11 +47,13 @@ const saveBlog = async (req, res) => {
 
 const updateBlog = async (req, res) => {
 
+    const blogToUpdate = req.params.id;
+
     try {
-        const blog = await Blog.findById(req.params.id);
+        const blog = await Blog.findById(blogToUpdate);
 
         if(!blog) {
-            res.status(404).json({success: false, message: `Blog with id of ${req.params.id} not found`});
+            res.status(404).json({success: false, message: `Blog with id of ${blogToUpdate} not found`});
         }
         const {title, body, author, published, imageUrl} = req.body;
 
@@ -64,6 +68,27 @@ const updateBlog = async (req, res) => {
         res.status(200).json({success: true, message: 'Blog updated successfully'});
 
     } catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Server Error'});
+    }
+}
+
+const deleteBlog = async (req, res) => {
+
+    try {
+
+        const blogToDelete = req.params.id;
+
+        const blog = await Blog.findById(blogToDelete);
+
+        if(!blog) {
+            res.status(404).json({success: false, message: 'Blog you want to delete do not exist'});
+        }
+
+        Blog.delete(blogToDelete);
+        res.status(200).json({success: true, message: "Blog deleted successfully"});
+
+    } catch(err) {
         console.log(err);
         res.status(500).json({success: false, message: 'Server Error'});
     }
