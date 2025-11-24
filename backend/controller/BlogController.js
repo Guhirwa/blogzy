b3c4fdbf06173eb705b42daf15eefd8440c4bf81
Guhirwa <christianguhirwa74@gmail.com>
2025-11-24 16:03:12 +0200

    feat(controller): implementing saveBlog method
    - validating completenes of the incoming data
    - provide consistent and helpful message

--- backend/controller/BlogController.js
+++ backend/controller/BlogController.js
@@ -25,3 +25,33 @@ const getBlogById = async (req, res) => {
         res.status(500).json({success: false, message: "Server Error"});
     }
 }
+
+const saveBlog = async (req, res) => {
+
+    try {
+        const {title, body, author, published, imageUrl} = req.body;
+
+        if(!title || !body || !author) {
+            res.status(400).json({success: false, message: 'Title, Body, Author are requeired'});
+        } else {
+            await Blog.save(req.body);
+            res.status(201).json({success: true, message: 'Blog saved successfully'});
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Server Error'});
+    }
+}
+
+const updateBlog = async (req, res) => {
+
+    try {
+        const blog = await Blog.findById(req.params.id);
+        if(!blog) {
+            res
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({success: false, message: 'Server Error'});
+    }
+}
